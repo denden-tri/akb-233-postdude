77cf34600294467bab03cb12f70e4221767e9ad5
Denden Triana <denden.triana@gmail.com>
2022-07-23 15:09:13 +0700

    make ResponseDialogFragment, add tab to ResponseDialogFragment, send button on RequestFragment expand ResponseDialogFragment

--- app/src/main/java/com/denztri/postdudeclient/ui/request/RequestFragment.java
+++ app/src/main/java/com/denztri/postdudeclient/ui/request/RequestFragment.java
@@ -9,17 +9,27 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-import com.denztri.postdudeclient.R;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.denztri.postdudeclient.R;
 import com.denztri.postdudeclient.databinding.FragmentRequestBinding;
+import com.denztri.postdudeclient.ui.response.ResponseDialogFragment;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class RequestFragment extends Fragment {
 
     private FragmentRequestBinding binding;
+
+    private BottomSheetBehavior sheetBehavior;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -49,11 +59,92 @@ public class RequestFragment extends Fragment {
 
             }
         });
+
+        ConstraintLayout constraintLayout = requireActivity().findViewById(R.id.res_bottom_sheet_constrain);
+        sheetBehavior = BottomSheetBehavior.from(constraintLayout);
+
+        showReqTab();
+        showResTab();
+
+        ViewCompat.setOnApplyWindowInsetsListener(view, (v, insets) -> {
+            boolean vis = insets.isVisible(WindowInsetsCompat.Type.ime());
+            Log.d("IME visible", String.valueOf(vis));
+
+            return insets;
+        });
+
+
+        binding.reqSendBtn.setOnClickListener(view1 -> sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED));
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void showResTab(){
+        ViewPager2 resPager = requireActivity().findViewById(R.id.res_pager);
+        TabLayout resTab = requireActivity().findViewById(R.id.res_tab);
+        ResponseDialogFragment.DemoCollectionAdapter demoCollectionAdapter = new ResponseDialogFragment.DemoCollectionAdapter(this);
+        resPager.setAdapter(demoCollectionAdapter);
+        new TabLayoutMediator(resTab, resPager,
+                (tab, position) -> {
+                    switch (position){
+                        case 0:
+                            tab.setText(R.string.response);
+                            break;
+                        case 1:
+                            tab.setText(R.string.headers);
+                            break;
+                        case 2:
+                            tab.setText(R.string.cookies);
+                            break;
+                    }
+                }
+        ).attach();
+    }
+
+    private void showReqTab(){
+        ViewPager2 reqPager = requireActivity().findViewById(R.id.req_pager);
+        TabLayout reqTab = requireActivity().findViewById(R.id.req_tab);
+        ReqAdapter reqAdapter = new ReqAdapter(this);
+        reqPager.setAdapter(reqAdapter);
+        new TabLayoutMediator(reqTab, reqPager,
+                (tab, position) -> {
+                    switch (position){
+                        case 0:
+                            tab.setText(R.string.query);
+                            break;
+                        case 1:
+                            tab.setText(R.string.header);
+                            break;
+                        case 2:
+                            tab.setText(R.string.auth);
+                            break;
+                        case 3:
+                            tab.setText(R.string.body);
+                            break;
+                    }
+                }
+        ).attach();
+    }
+
+    public static class ReqAdapter extends FragmentStateAdapter {
+        public ReqAdapter(Fragment fragment) {
+            super(fragment);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            // Return a NEW fragment instance in createFragment(int)
+            return new ResponseDialogFragment.DemoObjectFragment();
+        }
+
+        @Override
+        public int getItemCount() {
+            return 4;
+        }
     }
 }
---
+++ app/src/main/java/com/denztri/postdudeclient/ui/response/ResponseDialogFragment.java
@@ -0,0 +1,140 @@
+package com.denztri.postdudeclient.ui.response;
+
+import android.app.Dialog;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.denztri.postdudeclient.R;
+import com.denztri.postdudeclient.databinding.FragmentResponseDialogBinding;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class ResponseDialogFragment extends BottomSheetDialogFragment {
+    private FragmentResponseDialogBinding binding;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentResponseDialogBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        dialog.setOnShowListener(dialogInterface -> {
+            BottomSheetDialog bottomSheetDialog = (BottomSheetDialog) dialogInterface;
+            setupFullHeight(bottomSheetDialog);
+        });
+        return dialog;
+    }
+
+    public static class DemoCollectionAdapter extends FragmentStateAdapter {
+        public DemoCollectionAdapter(Fragment fragment) {
+            super(fragment);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            // Return a NEW fragment instance in createFragment(int)
+            return new DemoObjectFragment();
+        }
+
+        @Override
+        public int getItemCount() {
+            return 3;
+        }
+    }
+
+    // Instances of this class are fragments representing a single
+// object in our collection.
+    public static class DemoObjectFragment extends Fragment {
+        public static final String ARG_OBJECT = "object";
+
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                                 @Nullable Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_collection_object, container, false);
+        }
+
+        @Override
+        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+            Bundle args = getArguments();
+        }
+    }
+
+    private void setupFullHeight(BottomSheetDialog bottomSheetDialog) {
+        FrameLayout bottomSheet = bottomSheetDialog.findViewById(com.google.android.material.R.id.design_bottom_sheet);
+        assert bottomSheet != null;
+        BottomSheetBehavior<FrameLayout> behavior = BottomSheetBehavior.from(bottomSheet);
+        ViewGroup.LayoutParams layoutParams = bottomSheet.getLayoutParams();
+
+        int windowHeight = getWindowHeight();
+        if (layoutParams != null) {
+            layoutParams.height = windowHeight;
+        }
+
+        behavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+
+        bottomSheet.setLayoutParams(layoutParams);
+        behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+    }
+
+    private int getWindowHeight() {
+        // Calculate window height for fullscreen use
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        requireActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics.heightPixels;
+    }
+
+    private int getDpToPx(){
+        Resources r = getResources();
+        float px = TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                (float) 48.0,
+                r.getDisplayMetrics()
+        );
+
+        return  Math.round(px);
+    }
+
+}
