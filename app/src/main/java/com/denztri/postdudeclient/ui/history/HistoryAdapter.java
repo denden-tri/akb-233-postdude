95298acc860360f6d42b1be02b31492a7ec22c9f
Denden Triana <denden.triana@gmail.com>
2022-08-12 13:35:26 +0700

    clicking on recycler item now navigate to RequestFragment

--- app/src/main/java/com/denztri/postdudeclient/ui/history/HistoryAdapter.java
+++ app/src/main/java/com/denztri/postdudeclient/ui/history/HistoryAdapter.java
@@ -2,12 +2,15 @@ package com.denztri.postdudeclient.ui.history;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.denztri.postdudeclient.R;
@@ -50,6 +53,9 @@ public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.HistoryV
     public void onBindViewHolder(@NonNull HistoryViewHolder holder, int position) {
         holder.url.setText(historyModels.get(position).getUrl());
         holder.method.setText(historyModels.get(position).getHttpMethod());
+        holder.constraintLayout.setOnClickListener(view -> {
+            Navigation.findNavController(view).navigate(R.id.nav_request);
+        });
     }
 
     @Override
@@ -61,11 +67,12 @@ public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.HistoryV
     public static class HistoryViewHolder extends RecyclerView.ViewHolder{
         TextView method;
         TextView url;
-
+        ConstraintLayout constraintLayout;
         public HistoryViewHolder(@NonNull View itemView) {
             super(itemView);
             method = itemView.findViewById(R.id.history_row_method);
             url = itemView.findViewById(R.id.history_row_url);
+            constraintLayout = itemView.findViewById(R.id.history_row_root);
         }
     }
 }
